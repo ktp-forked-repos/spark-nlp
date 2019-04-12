35dffeacc6d22807f494441b497b16b64a009406
danilojsl <danilo@johnsnowlabs.com>
2019-04-11 20:24:58 -0500

    Adds accuracy and time metrics

--- src/test/scala/com/johnsnowlabs/nlp/evaluation/ner/NerDLEvaluation.scala
+++ src/test/scala/com/johnsnowlabs/nlp/evaluation/ner/NerDLEvaluation.scala
@@ -1,66 +1,40 @@
 package com.johnsnowlabs.nlp.evaluation.ner
 
-import java.sql.Struct
+import java.io.File
 
-import com.johnsnowlabs.nlp.{Annotation, AnnotatorType, DocumentAssembler, Finisher}
+import com.johnsnowlabs.nlp.DocumentAssembler
+import com.johnsnowlabs.nlp.SparkAccessor.spark.implicits._
 import com.johnsnowlabs.nlp.annotators.Tokenizer
 import com.johnsnowlabs.nlp.annotators.ner.NerConverter
 import com.johnsnowlabs.nlp.annotators.ner.dl.NerDLApproach
 import com.johnsnowlabs.nlp.annotators.sbd.pragmatic.SentenceDetector
 import com.johnsnowlabs.nlp.util.io.ResourceHelper
-import org.apache.avro.generic.GenericData.StringType
+import com.johnsnowlabs.util.{Benchmark, PipelineModels}
 import org.apache.spark.ml.{Pipeline, PipelineModel}
-import org.apache.spark.sql.{Column, Dataset, Row}
-import org.apache.spark.sql.expressions.UserDefinedFunction
-import org.apache.spark.sql.functions.{col, typedLit, udf}
-import org.apache.spark.sql.types.{ArrayType, MetadataBuilder, StructField, StructType}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.functions.{col, udf}
 import org.scalatest.FlatSpec
-
-import scala.collection.mutable.ArrayBuffer
 
 
 class NerDLEvaluation extends FlatSpec {
 
   private val trainFile = "/Users/dburbano/JupyterNotebooks/JSL/Assessment/eng.train"
-  private val trainDataSet = getTrainDataSetWithTokens(trainFile)
-  trainDataSet.show(5, false)
-  private val nerModel = getNerModel
-  private val predictionDataSet = nerModel.transform(trainDataSet)
-    .select("ground_truth", "ner.result")
-  predictionDataSet.show(5, false)
-  predictionDataSet.printSchema()
-  private lazy val labels = getEntitiesLabels(predictionDataSet, "ground_truth")
-  println(labels)
-  private val evaluationDataSet = getEvaluationDataSet(predictionDataSet)
-  evaluationDataSet.show(5, false)
-  evaluationDataSet.printSchema()
-  evaluationDataSet.withColumn("predictionIndex", customNer(col("prediction")))
-  evaluationDataSet.show()
+  private val testFile = "/Users/dburbano/JupyterNotebooks/JSL/Assessment/eng.testa"
+  evaluateDataSet("Training Dataset", trainFile)
+  evaluateDataSet("Testing Dataset", testFile)
 
   def getNerPipeline: Pipeline = {
-
-    val documentAssembler = new DocumentAssembler()
-      .setInputCol("text")
-      .setOutputCol("document")
-      .setTrimAndClearNewLines(false)
-
-    val sentenceDetector = new SentenceDetector()
-      .setInputCols(Array("document"))
-      .setOutputCol("sentence")
-
-    val tokenizer = new Tokenizer()
-      .setInputCols(Array("sentence"))
-      .setOutputCol("token")
-      .setPrefixPattern("\\A([^\\s\\p{L}$\\.']*)")
 
     val nerTagger = new NerDLApproach()
       .setInputCols(Array("sentence", "token"))
       .setLabelColumn("label")
       .setOutputCol("ner")
-      .setMaxEpochs(1)
+      .setMaxEpochs(10)
       .setRandomSeed(0)
       .setVerbose(2)
-      .setEmbeddingsSource("glove.6B.100d.txt", 100, 2)
+      .setEmbeddingsSource("/Users/dburbano/JupyterNotebooks/JSL/Assessment/glove.6B.100d.txt", 100, 2)
+      .setExternalDataset(trainFile)
 
     val converter = new NerConverter()
       .setInputCols(Array("document", "token", "ner"))
@@ -68,9 +42,6 @@ class NerDLEvaluation extends FlatSpec {
 
     val pipeline = new Pipeline().setStages(
       Array(
-        documentAssembler,
-        sentenceDetector,
-        tokenizer,
         nerTagger,
         converter))
 
@@ -104,8 +75,18 @@ class NerDLEvaluation extends FlatSpec {
 
   }
 
-  def getNerModel: PipelineModel = {
-    PipelineModel.load("/Users/dburbano/PycharmProjects/TestingJSL/model/ner_dl_model")
+  def getNerModel(trainDataSet: Dataset[_]): PipelineModel = {
+    if (new File("./ner_dl_model").exists()) {
+      PipelineModel.load("./ner_dl_model")
+    } else {
+      val nerPipeline = getNerPipeline
+      var model: PipelineModel = null
+      Benchmark.measure("Time to train") {
+        model = nerPipeline.fit(trainDataSet)
+      }
+      model.write.overwrite().save("./ner_dl_model")
+      model
+    }
   }
 
   def getTrainDataSetWithTokens(pathDataSet: String): Dataset[_] = {
@@ -118,6 +99,7 @@ class NerDLEvaluation extends FlatSpec {
     trainDataSet = trainDataSet.filter("text != '-DOCSTART-'")
     val pipeline = getTokenPipeline
     pipeline.fit(trainDataSet).transform(trainDataSet)
+      .filter("ground_truth is not null")
   }
 
   def getEntitiesLabels(dataSet: Dataset[_], column: String): List[String] = {
@@ -127,27 +109,62 @@ class NerDLEvaluation extends FlatSpec {
     labels.asInstanceOf[List[String]]
   }
 
-  def getEvaluationDataSet(dataSet: Dataset[_]): Dataset[_] = {
-    //  predictionDataSet = predictionDataSet.withColumn("labelIndex",
-    //    when(col("ground_truth") =!= "", labels.indexOf(col("ground_truth"))).otherwise(0))
-    //private val getLabelIndexUDF = udf{label: String => label.toLowerCase}
-    //private val getLabelIndexUDF = udf(toLower _)
+  def getEvaluationDataSet(dataSet: Dataset[_], labels: List[String]): Dataset[_] = {
 
     dataSet
       .withColumn("labelIndex", getLabelIndex(labels)(col("ground_truth")))
       .withColumnRenamed("result", "prediction")
-      //.withColumn("prediction", col("result").cast("string"))
-      //.withColumn("prediction", customNer(col("result")))
+      .withColumnRenamed("ground_truth", "label")
+      .withColumn("prediction", col("prediction").cast("string"))
+      .withColumn("prediction", cleanPrediction(col("prediction")))
+      .withColumn("predictionIndex", getLabelIndex(labels)(col("prediction")))
   }
 
   private def getLabelIndex(labels: List[String]) = udf { label: String =>
-    val index = labels.indexOf(label)
-    index.toString
+      val index = labels.indexOf(label)
+      index.toDouble
   }
 
-  private def customNer = udf { ner: Array[String] =>
-    ner.head
+  private def cleanPrediction = udf { label: String =>
+    label.replace("[", "").replace("]", "")
   }
 
+  private def computeAccuracy(dataSet: Dataset[_], labels: List[String]): Unit = {
+    val predictionLabelsRDD = dataSet.select("predictionIndex", "labelIndex")
+      .map(r => (r.getDouble(0), r.getDouble(1)))
+    val metrics = new MulticlassMetrics(predictionLabelsRDD.rdd)
+    val accuracy = (metrics.accuracy * 1000).round / 1000.toDouble
+    println(s"Accuracy = $accuracy")
+    computeAccuracyByEntity(metrics, labels)
+  }
+
+  private def computeAccuracyByEntity(metrics: MulticlassMetrics, labels: List[String]): Unit = {
+    val predictedLabels = metrics.labels
+    predictedLabels.foreach{predictedLabel =>
+      val entity = labels(predictedLabel.toInt)
+      val precision = (metrics.precision(predictedLabel) * 1000).round / 1000.toDouble
+      val recall = (metrics.recall(predictedLabel) * 1000).round / 1000.toDouble
+      val f1Score = (metrics.fMeasure(predictedLabel) * 1000).round / 1000.toDouble
+      println(s"$entity: Precision = $precision, Recall = $recall, F1-Score = $f1Score")
+    }
+  }
+
+  private def evaluateDataSet(dataSetType: String, dataSetFile: String): Unit = {
+    val trainDataSet = getTrainDataSetWithTokens(dataSetFile)
+    val nerModel = getNerModel(trainDataSet)
+    var predictionDataSet: Dataset[_] = PipelineModels.dummyDataset
+    println(s"Accuracy for $dataSetType")
+    Benchmark.measure("Time to transform") {
+      predictionDataSet = nerModel.transform(trainDataSet)
+        .select("ground_truth", "ner.result")
+    }
+    Benchmark.measure("Time to show prediction dataset") {
+      predictionDataSet.show(5)
+    }
+    val labels = getEntitiesLabels(predictionDataSet, "ground_truth")
+    println("Entities: " + labels)
+    val evaluationDataSet = getEvaluationDataSet(predictionDataSet, labels)
+    computeAccuracy(evaluationDataSet, labels)
+  }
 
 }
